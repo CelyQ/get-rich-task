79670cf66cda579951c8e3ab6c9861effac55efd
CelyQ <paul.opmann@gmail.com>
2023-01-03 17:15:42 +0900

    cards finished

--- src/components/Cards/Affiliate/affiliateCard.tsx
+++ src/components/Cards/Affiliate/affiliateCard.tsx
@@ -1,10 +1,26 @@
 import { CardContainer } from '../../../containers'
-import AffiliateSVG from './affiliateSVG'
+import affiliateSVG from '../../../assets/affiliate.svg'
+import affiliateActiveSVG from '../../../assets/affiliateActive.svg'
 
 export default function AffiliateCard({ onClick }: { onClick: () => void }) {
   return (
     <CardContainer text="I am the Affiliate" onClick={onClick}>
-      <AffiliateSVG />
+      <div className="relative w-[111px] h-[155px] group/card">
+        <img
+          className="absolute transition-all duration-300 group-hover/card:opacity-0 group-hover/card:scale-110"
+          src={affiliateSVG}
+          alt=""
+          width={111}
+          height={155}
+        />
+        <img
+          className="absolute transition-all duration-300 opacity-0 group-hover/card:opacity-100 group-hover/card:scale-110"
+          src={affiliateActiveSVG}
+          alt=""
+          width={111}
+          height={155}
+        />
+      </div>
     </CardContainer>
   )
 }
--- src/components/Cards/Investor/investorCard.tsx
+++ src/components/Cards/Investor/investorCard.tsx
@@ -1,10 +1,26 @@
 import { CardContainer } from '../../../containers'
-import InvestorSVG from './investorSVG'
+import InvestorSVG from '../../../assets/investor.svg'
+import InvestorActiveSVG from '../../../assets/investorActive.svg'
 
 export default function InvestorCard({ onClick }: { onClick: () => void }) {
   return (
     <CardContainer text="I am the Investor" onClick={onClick}>
-      <InvestorSVG />
+      <div className="relative w-[111px] h-[155px] group/card">
+        <img
+          className="absolute transition-all duration-300 group-hover/card:opacity-0 group-hover/card:scale-110"
+          src={InvestorSVG}
+          alt=""
+          width={111}
+          height={155}
+        />
+        <img
+          className="absolute transition-all duration-300 opacity-0 group-hover/card:opacity-100 group-hover/card:scale-110"
+          src={InvestorActiveSVG}
+          alt=""
+          width={111}
+          height={155}
+        />
+      </div>
     </CardContainer>
   )
 }
