819bc4068c306dc0627ca5fdaba27af796210b37
CelyQ <paul.opmann@gmail.com>
2023-01-03 16:50:04 +0900

    2 svgs added + animations

--- src/components/Cards/Contributor/contributorCard.tsx
+++ src/components/Cards/Contributor/contributorCard.tsx
@@ -1,10 +1,26 @@
 import { CardContainer } from '../../../containers'
-import ContributorSVG from './contributorSVG'
+import contributorSVG from '../../../assets/contributor.svg'
+import contributorActiveSVG from '../../../assets/contributorActive.svg'
 
 export default function ContributorCard({ onClick }: { onClick: () => void }) {
   return (
     <CardContainer text="I am the Contributor" onClick={onClick}>
-      <ContributorSVG />
+      <div className="relative w-[111px] h-[155px] group/card">
+        <img
+          className="absolute transition-all duration-300 group-hover/card:opacity-0 group-hover/card:scale-110"
+          src={contributorSVG}
+          alt=""
+          width={111}
+          height={155}
+        />
+        <img
+          className="absolute transition-all duration-300 opacity-0 group-hover/card:opacity-100 group-hover/card:scale-110"
+          src={contributorActiveSVG}
+          alt=""
+          width={111}
+          height={155}
+        />
+      </div>
     </CardContainer>
   )
 }
--- src/components/Cards/Player/playerCard.tsx
+++ src/components/Cards/Player/playerCard.tsx
@@ -1,10 +1,26 @@
 import { CardContainer } from '../../../containers'
-import PlayerSVG from './playerSVG'
+import PlayerSVG from '../../../assets/player.svg'
+import PlayerActiveSVG from '../../../assets/playerActive.svg'
 
 export default function PlayerCard({ onClick }: { onClick: () => void }) {
   return (
     <CardContainer text="I am the Player" onClick={onClick}>
-      <PlayerSVG />
+      <div className="relative w-[111px] h-[155px] group/card">
+        <img
+          className="absolute transition-all duration-300 group-hover/card:opacity-0 group-hover/card:scale-110"
+          src={PlayerSVG}
+          alt=""
+          width={111}
+          height={155}
+        />
+        <img
+          className="absolute transition-all duration-300 opacity-0 group-hover/card:opacity-100 group-hover/card:scale-110"
+          src={PlayerActiveSVG}
+          alt=""
+          width={111}
+          height={155}
+        />
+      </div>
     </CardContainer>
   )
 }
